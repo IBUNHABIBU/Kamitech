17360099b5b74079831117b2d437971cab138ac7
IBUNHABIBU <ibunhabib@gmail.com>
2024-12-18 20:54:55 +0300

    Add nav toggle

--- app/javascript/controllers/pong_controller.js
+++ app/javascript/controllers/pong_controller.js
@@ -5,11 +5,9 @@ export default class extends Controller {
   static targets = ["button"]
   connect() {
     this.toggle();
-    console.log("Hello, Stimulus!", this.element);
   }
 
   toggle() {
     this.element.classList.toggle("open");
-    console.log('toggle');
   }
 }
