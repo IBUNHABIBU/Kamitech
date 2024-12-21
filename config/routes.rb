2042056f3b00b2fceb1299034ec4b61f9b1cbc6f
IBUNHABIBU <ibunhabib@gmail.com>
2024-12-21 23:01:45 +0300

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'contact_us', to: 'contact_form#index', as: 'contact_us'
+  get 'contact_us', to: 'contact_form#index', as: 'contact_kamich_technologies_tz'
 
   get 'contact_form/new'
   post 'contact_form/create'
-  get 'about_us', to: 'about#index', as: 'about_kamich_technologies'
+  get 'about_us', to: 'about#index', as: 'about_kamich_technologies_tz'
   resources :partners
   resources :projects
   resources :ourservices
