cb5b30f4e5a435ff5b8a2769a498879011e33d5b
IBUNHABIBU <ibunhabib@gmail.com>
2024-12-21 23:04:39 +0300

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :projects
   resources :ourservices
   
-  get 'ourservices', to: 'ourservice#index', as: 'about_kamich_technologies_tz'
+  get 'ourservices', to: 'ourservice#index', as: 'kamich_technologies_tz_services'
   
   get 'kashg', to: 'cpanel#index', as: 'kashg'
   resources :hero_images
