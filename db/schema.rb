f70c796010673f8cdeb9c5df59b973068c5922a6
IBUNHABIBU <ibunhabib@gmail.com>
2024-12-23 14:25:32 +0300

    Add friendly id gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_23_110640) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_23_112412) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -74,6 +74,8 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_23_110640) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "slug"
+    t.index ["slug"], name: "index_ourservices_on_slug", unique: true
   end
 
   create_table "partners", force: :cascade do |t|
@@ -90,6 +92,8 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_23_110640) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_projects_on_slug", unique: true
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
