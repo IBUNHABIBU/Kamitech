58d1fa9ef51d9a3feb2b1f3b1f671e0407bbb927
IBUNHABIBU <ibunhabib@gmail.com>
2024-12-23 14:23:01 +0300

    Add friendly id gem

--- app/models/ourservice.rb
+++ app/models/ourservice.rb
@@ -1,8 +1,8 @@
 class Ourservice < ApplicationRecord
+    extend FriendlyId
     has_one_attached :image
     belongs_to :user
     validates :Title, presence: true, length: { minimum: 5 }
     validates :description, presence: true, length: { minimum: 10 }
-    extend FriendlyId
     friendly_id :Title, use: :slugged
 end
